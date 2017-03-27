4d716a3159c7b2f9e0b256a5f352f00dc77799c5
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-27 12:47:08 +0200

    Do profile

--- qtmain.cpp
+++ qtmain.cpp
@@ -1,3 +1,4 @@
+#include <QTimer>
 #include <QApplication>
 #include "qttesthistogrammenudialog.h"
 
@@ -5,6 +6,12 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   ribi::thst::QtMenuDialog w;
+
+  if (argc == 2)
+  {
+    QTimer::singleShot(1000, &w, SLOT(close()));
+  }
+
   w.show();
   return a.exec();
 }
